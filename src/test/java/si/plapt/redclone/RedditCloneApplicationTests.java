d5c8b7dcc474a3c6afd274702a1ed2d5846df889
Bogdan Lipuš <bolipus@gmail.com>
2020-05-12 15:43:13 +0200

    Initial commit

---
+++ src/test/java/si/plapt/redclone/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package si.plapt.redclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
