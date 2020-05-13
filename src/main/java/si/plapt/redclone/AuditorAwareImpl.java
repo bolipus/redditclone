7a86fc85f724f740b09dfe942e36e3eea5b726a5
Bogdan Lipuš <bolipus@gmail.com>
2020-05-13 21:14:18 +0200

    Correct AuditorAware

--- src/main/java/si/plapt/redclone/AuditorAwareImpl.java
+++ src/main/java/si/plapt/redclone/AuditorAwareImpl.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
-public class AuditorAwareImpl implements AuditorAware {
+public class AuditorAwareImpl implements AuditorAware<String> {
 
   @Override
   public Optional<String> getCurrentAuditor() {
--- src/main/java/si/plapt/redclone/config/JpaConfig.java
+++ src/main/java/si/plapt/redclone/config/JpaConfig.java
@@ -1,15 +1,18 @@
 package si.plapt.redclone.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import si.plapt.redclone.AuditorAwareImpl;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class JpaConfig {
   
   @Bean
-  public AuditorAware auditorAware(){    
+  public AuditorAware<String> auditorAware(){    
     return new AuditorAwareImpl();
   }
 }
